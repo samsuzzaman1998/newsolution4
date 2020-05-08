4457b1bdbdedcbfc8561306e10a312379d390d8a
samsuzzaman1998 <64584061+samsuzzaman1998@users.noreply.github.com>
2020-05-08 16:41:31 +0600

    html js

---
+++ main.js
@@ -0,0 +1,15 @@
+(function () {
+
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+  for (var i = 0; i < names.length; i++) {
+    var firstLetter = names[i].charAt(0);
+    var frstLtrLwrCse = firstLetter.toLowerCase();
+
+    if (firstLetter === "J" || frstLtrLwrCse === "j") {
+      console.log("Goodbye" + " " + names[i]);
+    } else {
+      console.log("Hello" + " " + names[i]);
+    }
+  }
+})();
